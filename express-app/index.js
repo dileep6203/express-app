e65ed0130a616e8ac656326efdb444c03cca4ad4
dileep6203 <dileep.6203@gmail.com>
2024-03-20 01:08:05 +0000

    editing index.js file

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Express World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 const PORT = 8080
@@ -10,3 +10,4 @@ const PORT = 8080
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
