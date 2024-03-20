0338b2e16fcca0aed7147f1cb9cd2f6c99fdec84
dileep6203 <dileep.6203@gmail.com>
2024-03-20 00:24:19 +0000

    editing index.js for testing watchtower

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello Express World!</h1>'i
 })
 
 const PORT = 8080
